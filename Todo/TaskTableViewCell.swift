7f9de6920a6977c668a4c6225f4ca5109143beb6
Jany <my.maithi@gmail.com>
2017-08-04 22:16:36 +0700

    init Task TableViewCell to control what will display on each Cell

---
+++ Todo/TaskTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  TaskTableViewCell.swift
+//  Todo
+//
+//  Created by My Mai on 04/08/2017.
+//  Copyright © 2017 JanyMai. All rights reserved.
+//
+
+import UIKit
+
+class TaskTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
